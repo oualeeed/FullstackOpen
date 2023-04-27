56db79bea61de1a392ddfe3f7e076d9dfe1629e3
Oualid elf <oualid.elferaoui@gmail.com>
2023-04-27 16:03:56 +0100

    1.6 : unicafe step 1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Button = ({text ,handler}) =>  <button onClick={handler}>{text}</button>
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Give feedback</h1>
+      <Button text='good' handler={() => setGood(good + 1)}/>
+      <Button text='neutral' handler={() => setNeutral(neutral + 1)}/>   
+      <Button text='bad' handler={() => setBad(bad + 1)}/>
+      <h2>Statistics</h2>     
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  )
 }
 
 export default App;
